1a0b7e4865b1d4cb48e3552e68650ae6e6fd93d8
jmpark6846 <jmpark6846@gmail.com>
2018-04-27 17:53:54 +0900

    load more 추가

--- src/App.js
+++ src/App.js
@@ -14,36 +14,46 @@ topic
 가장 starred
 */
 const GITHUB_URL = 'https://api.github.com/search/repositories?q=language:javascript&sort=stars&order=desc&page=1&per_page=15'
+const BASE_URL = 'https://api.github.com/search/repositories';
+const QUERY_LANGUAGE='?q=language:';
+const DEFAULT_LANGUAGE='javascript';
+const QUERY_CONDITION='&sort=stars&order=desc';
+const QUERY_PAGE =  '&page=';
+const QUERY_PP = '&per_page=15';
 class App extends Component {
   constructor(props){
     super(props);
     this.state={
       repos:[],
-      language:''
+      language:DEFAULT_LANGUAGE,
+      page:1
     }
 
-    this.getWeeklyHotRepository = this.getWeeklyHotRepository.bind(this);
+    this.getHotRepository = this.getHotRepository.bind(this);
   }
 
   componentWillMount() {
-    this.getWeeklyHotRepository();
+    this.getHotRepository(this.state.page);
   }
 
-  getWeeklyHotRepository(){
+  getHotRepository(page){
     const { repos, language } = this.state;
+    const GITHUB_URL = `${BASE_URL}${QUERY_LANGUAGE}${language}${QUERY_CONDITION}${QUERY_PAGE}${page}${QUERY_PP}`;
+
 
     axios.get(GITHUB_URL)
-      .then(response=>this.setState({ repos: response.data.items }))
+      .then(response=>{
+        this.setState((prevState)=>{ return { repos: [...prevState.repos, ...response.data.items]}})
+      })
       .catch(error=> console.log(error));
   }
 
-  
   render() {
-    const { repos } = this.state;
+    const { repos, page } = this.state;
     console.log(repos);
     return (
       <div className="App">
-      <div className="">
+        <div className="repositoryList">
         {repos.map((repo) =>  
           <div key={repo.id} className="p-4 repository">
             <a href={repo.html_url} target="_blank">
@@ -51,8 +61,8 @@ class App extends Component {
                 <h1 className="repo-name mb-2">{repo.name}</h1>
                 <span className="repo-owner ml-2">{repo.owner.login}</span>
                 <div className="float-right mt-3">
-                  <span><i class="fas fa-star"></i> {repo.stargazers_count}</span>
-                  <span className="ml-3"><i class="fas fa-code-branch"></i> {repo.forks_count}</span>
+                  <span><i className="fas fa-star"></i> {repo.stargazers_count}</span>
+                  <span className="ml-3"><i className="fas fa-code-branch"></i> {repo.forks_count}</span>
                 </div>
               </div>
               <div className="repo-desc">{repo.description}</div>
@@ -60,6 +70,7 @@ class App extends Component {
           </div>
         )}
         </div>
+        <button onClick={()=>this.getHotRepository(page+1)}>Load more</button>
       </div>
     )
   }
