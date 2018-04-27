a3e48fb6961c671a80a04769e0698075bde548a5
jmpark6846 <jmpark6846@gmail.com>
2018-04-27 17:03:16 +0900

    repo list 수정. fontawesome 추가

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ topic
 지난주 업데이트
 가장 starred
 */
-const GITHUB_URL = 'https://api.github.com/search/repositories?q=topic=react+language:javascript&sort=stars&order=desc'
+const GITHUB_URL = 'https://api.github.com/search/repositories?q=language:javascript&sort=stars&order=desc&page=1&per_page=15'
 class App extends Component {
   constructor(props){
     super(props);
@@ -42,11 +42,20 @@ class App extends Component {
     const { repos } = this.state;
     console.log(repos);
     return (
-      <div>
+      <div className="App container">
       {repos.map((repo) =>  
-        <div className="repository">
-          <div className="repo-name">{repo.name}</div>
-          <div calssNAme="repo-desc">{repo.description}</div>
+        <div key={repo.id} className="mb-2 p-3 repository">
+          <a href={repo.html_url} target="_blank">
+            <div>
+              <h1 className="repo-name mb-1">{repo.name}</h1>
+              <span>{repo.owner.login}</span>
+              <div className="float-right mt-2">
+                <span className="ml-2"><i class="fas fa-star"></i>{repo.stargazers_count}</span>
+                <span className="ml-2"><i class="fas fa-code-branch"></i>{repo.forks_count}</span>
+              </div>
+            </div>
+            <div className="repo-desc">{repo.description}</div>
+          </a>
         </div>
       )}
       </div>
