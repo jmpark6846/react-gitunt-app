1bc77f047aaa1d28b300028959a7d2f414ac1798
jmpark6846 <jmpark6846@gmail.com>
2018-04-27 16:22:32 +0900

    github api로 리스트 fetch

--- src/App.js
+++ src/App.js
@@ -1,21 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+/*
+Search
+RepoList
+  Repository
+  Repository
+  Repository
 
+
+topic 
+지난주 업데이트
+가장 starred
+*/
+const GITHUB_URL = 'https://api.github.com/search/repositories?q=topic=react+language:javascript&sort=stars&order=desc'
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      repos:[],
+      language:''
+    }
+
+    this.getWeeklyHotRepository = this.getWeeklyHotRepository.bind(this);
+  }
+
+  componentWillMount() {
+    this.getWeeklyHotRepository();
+  }
+
+  getWeeklyHotRepository(){
+    const { repos, language } = this.state;
+
+    axios.get(GITHUB_URL)
+      .then(response=>this.setState({ repos: response.data.items }))
+      .catch(error=> console.log(error));
+  }
+
+
   render() {
+    const { repos } = this.state;
+    console.log(repos);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+      {repos.map((repo) =>  
+        <div className="repository">
+          <div className="repo-name">{repo.name}</div>
+          <div calssNAme="repo-desc">{repo.description}</div>
+        </div>
+      )}
       </div>
-    );
+    )
   }
 }
-
 export default App;
